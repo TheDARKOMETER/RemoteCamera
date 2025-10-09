eba6cc0305e8802290780f1c711dd45f60b10136
TheDARKOEMTER <thebeangodzilla22@gmail.com>
2025-10-10 05:43:34 +0800

    Added Take Photo Function

--- app/src/main/java/com/example/remotecamera/MainActivity.java
+++ app/src/main/java/com/example/remotecamera/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.remotecamera;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.video.Recording;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
@@ -15,21 +28,55 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.remotecamera.databinding.ActivityMainBinding;
+import com.google.common.util.concurrent.ListenableFuture;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
 
     private static ActivityMainBinding viewBinding;
     private static final String TAG = "RemoteCameraApp";
-    private static final String fileNameFormat = "yyyy-MM-dd-HH-mm-ss-SSS";
+    private static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
     private static final String[] REQUIRED_PERMISSIONS;
     private ImageCapture imageCapture;
     private Recording recording;
     private ExecutorService cameraExecutor;
+    private ActivityResultLauncher<String[]> activityResultLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.RequestMultiplePermissions(),
+                    new ActivityResultCallback<Map<String, Boolean>>() {
+                        @Override
+                        public void onActivityResult(Map<String, Boolean> permissions) {
+                            boolean permissionGranted = true;
+
+                            for (Map.Entry<String,Boolean> entry : permissions.entrySet()) {
+                                String key = entry.getKey();
+                                Boolean value = entry.getValue();
+                                if (Arrays.asList(REQUIRED_PERMISSIONS).contains(key) && !value) {
+                                    permissionGranted = false;
+                                    break;
+                                }
+                            }
+
+                            if (!permissionGranted) {
+                                Toast.makeText(getBaseContext(),
+                                        "Permission request denied",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                startCamera();
+                            }
+                        }
+                    }
+            );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +103,78 @@ public class MainActivity extends AppCompatActivity {
         cameraExecutor.shutdown();
     }
 
-    private void takePhoto() {}
+    private void takePhoto() {
+
+        // Get a stable reference of the modifiable image capture use case
+        ImageCapture imageCapture = this.imageCapture;
+        if (imageCapture == null) {
+            return;
+        }
+
+        // Create time stamped name and MediaStore entry.
+        String name = new SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                .format(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/RemoteCamera-Image");
+        }
+
+
+        // Create output options object which contains file + metadata
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(
+                getContentResolver(), MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                .build();
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+                outputFileOptions,
+                ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        String message = "Photo capture succeeded: " + outputFileResults.getSavedUri();
+                        Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, message);
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Log.e(TAG, "Photo capture failed: " + exception.getMessage());
+                    }
+                }
+        );
+
+    }
     private void captureVideo() {}
-    private void startCamera() {}
-    private void requestPermissions() {}
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            ProcessCameraProvider cameraProvider;
+            try {
+                cameraProvider = cameraProviderFuture.get();
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            Preview preview = new Preview.Builder().build();
+            preview.setSurfaceProvider(viewBinding.viewFinder.getSurfaceProvider());
+            imageCapture = new ImageCapture.Builder().build();
+            CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+            try {
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle(this,cameraSelector,preview, imageCapture);
+            } catch(Exception e) {
+                Log.e(TAG, "Use case binding failed", e);
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+    private void requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS);
+    }
 
     private boolean allPermissionsGranted() {
        for (String permission : REQUIRED_PERMISSIONS) {
